fc3e25d91419516f57df481fe8e6603a5f671409
user <user@DESKTOP-FN026Q4>
2024-04-06 15:51:50 +0300

    Add project files.

---
+++ Lab1Ex6/Program.cs
@@ -0,0 +1,17 @@
+﻿//Ex 6
+//Scrieti un program care va verifica daca un numar citit de la tastatura este par sau
+//impar
+// In cazul in care numarul este par, programul va afisa “par” iar in caz contrar, “impar”.
+
+
+Console.WriteLine("Scrieti un numar");
+int numar = int.Parse(Console.ReadLine());
+
+if (numar % 2 == 0)
+{
+    Console.WriteLine("Numarul dv este par");
+}
+else
+{
+    Console.WriteLine("Numarul dv. est imnpar");
+}
